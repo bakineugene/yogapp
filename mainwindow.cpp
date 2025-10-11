0f9a33ba3105cae12d11937e0ae286ddfc7cce05
Eugene Bakin <bakin.eugene@ya.ru>
2025-10-11 13:12:00 +0500

    application template

---
+++ mainwindow.cpp
@@ -0,0 +1,78 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QDir>
+#include <QFileDialog>
+
+QPixmap ResizeImgToFit(const QPixmap &image, int window_width, int window_height) {
+    double width_ratio = double(image.width()) / window_width;
+    double height_ratio = double(image.height()) / window_height;
+
+    if ( width_ratio > height_ratio ) {
+        return image.scaledToWidth(window_width);
+    } else {
+        return image.scaledToHeight(window_height);
+    }
+}
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui_(new Ui::MainWindow) {
+    ui_->setupUi(this);
+
+    SetDirectory(":/cats/images");
+}
+
+MainWindow::~MainWindow() {
+    delete ui_;
+}
+
+void MainWindow::UpdateImage() {
+    auto pixmap = ResizeImgToFit(GetCurrentFile(), width(), height());
+    picture_label_.setPixmap(pixmap);
+    int label_x = (width() - pixmap.width()) / 2;
+    int label_y = (height() - pixmap.height()) / 2;
+
+    picture_label_.setGeometry(label_x, label_y, pixmap.width(), pixmap.height());
+}
+
+void MainWindow::SetDirectory(const QString &directory) {
+    current_file_index_ = 0;
+    images_directory_ = QDir{directory};
+
+    for (auto file : images_directory_.entryList()) {
+        QPixmap pixmap{images_directory_.filePath(file)};
+
+        if (!pixmap.isNull()) {
+            files_list_.append(pixmap);
+        }
+    }
+
+    UpdateImage();
+    UpdateEnabled();
+}
+
+QPixmap MainWindow::GetCurrentFile() {
+    if (files_list_.isEmpty()) return QPixmap{};
+
+    return files_list_[current_file_index_];
+}
+
+void MainWindow::resizeEvent(QResizeEvent*) {
+    UpdateImage();
+}
+
+void MainWindow::UpdateEnabled() {
+    ui_->btn_left->setEnabled(current_file_index_ > 0);
+    ui_->btn_right->setEnabled(current_file_index_ < files_list_.size() - 1);
+}
+
+void MainWindow::on_btn_right_clicked() {
+    ++current_file_index_;
+    UpdateImage();
+    UpdateEnabled();
+}
+
+void MainWindow::on_btn_left_clicked() {
+    --current_file_index_;
+    UpdateImage();
+    UpdateEnabled();
+}
---
+++ mainwindow.h
@@ -0,0 +1,55 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QLabel>
+#include <QDir>
+#include <QString>
+#include <QFileDialog>
+#include <QTimer>
+#include <QList>
+#include <QPixmap>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    void SetPixmap(const QPixmap pixmap) {
+        active_pixmap_ = pixmap;
+    }
+
+    void UpdateImage();
+    void SetDirectory(const QString& directory);
+    QPixmap GetCurrentFile();
+
+private slots:
+    void on_btn_right_clicked();
+    void on_btn_left_clicked();
+
+private:
+    void resizeEvent(QResizeEvent *event) override;
+    void UpdateEnabled();
+
+private:
+    // TODO: single struct
+    QDir images_directory_;
+    size_t current_file_index_{};
+    QList<QPixmap> files_list_{};
+
+    Ui::MainWindow *ui_;
+    QLabel picture_label_{this};
+    QPixmap active_pixmap_;
+    QTimer timer{this};
+
+};
+#endif // MAINWINDOW_H
